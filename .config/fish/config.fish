d7b4b9171f844740b31306d0538d2036f138e777
Magnus Watn <magnus@watn.no>
2025-12-23 14:53:49 +0100

    Set `exclude-newer` for uv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+
+# Exclude dependencies newer than 14 days
+set -gx UV_EXCLUDE_NEWER P14D

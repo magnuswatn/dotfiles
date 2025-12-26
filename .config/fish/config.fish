955f8895439405b60dbb61e5f935f0480b3d33dc
Magnus Watn <magnus@watn.no>
2025-12-26 15:24:19 +0100

    Set `PIP_REQUIRE_VIRTUALENV`
    (Don't really use pip, but why not?)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,7 @@ starship init fish | source
 
 # Exclude dependencies newer than 14 days
 set -gx UV_EXCLUDE_NEWER P14D
+
+# Don't allow installation of packages into
+# the global Python env
+set -gx PIP_REQUIRE_VIRTUALENV 1

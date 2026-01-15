1e99cbd685853a2a52a49745b1129624fbb27482
Magnus Watn <magnus.horsgard.watn@nhn.no>
2026-01-15 16:31:10 +0100

    Move uv stuff into uv config
    It was nice having the cooldown setting in an env variable when uv
    didn't support disabling it for packages from a private repo (so that
    the env variable could be removed when working on such packages), but
    now that it does, that config should be in the uv config file.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,9 +3,6 @@ if status is-interactive
 end
 starship init fish | source
 
-# Exclude dependencies newer than 14 days
-set -gx UV_EXCLUDE_NEWER P14D
-
 # Don't allow installation of packages into
 # the global Python env
 set -gx PIP_REQUIRE_VIRTUALENV 1
